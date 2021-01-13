4715ecd6b3dd1f5a9ec120bfafbd1601b05e074a
sandeepshakya2019 <sandeepshakya2015@gmail.com>
2021-01-13 22:59:46 +0530

    sandeep

--- src/Components/Javascript/NextGen.js
+++ src/Components/Javascript/NextGen.js
@@ -23,11 +23,53 @@ function NextGen({ basicclickFunction }) {
           <h1 className="heading">Next Genration Syntax</h1>
         </center>
         <div className="defination">
-          <p className="define-heading">ES6 Syntax</p>
+          <p className="define-heading"> ⭐ ES6 Syntax</p>
           <br />
           <br />
           <p className="define-text">
             We are going to study about the some ES6 Syntax in this Section
+          </p>
+        </div>
+        <div className="defination">
+          <p className="define-heading"> ⭐ Let and Const</p>
+          <br />
+          <br />
+          <p className="define-text">
+            Let and Const is new way to declare the variable in ES6 Syntax. Here
+            let keyword is used when we want to use the variable and Const
+            keyword used when we have to define the constant value
+          </p>
+          <p className="define-text">
+            1) Let and Const doesn't support Hoisiting and var keyword does if
+            you want to learn more about the Hoisiting i will leave the link for
+            you below of W3 schools
+            <br />
+            <a href="https://www.w3schools.com/js/js_hoisting.asp">
+              https://www.w3schools.com/js/js_hoisting.asp
+            </a>
+            <br />
+            2) Can acces with in the Block (Block Scope ) unlike var keyword
+          </p>
+        </div>
+        <div className="defination">
+          <p className="define-heading"> ⭐ Arrow Function</p>
+          <br />
+          <br />
+          <p className="define-text">
+            Let and Const is new way to declare the variable in ES6 Syntax. Here
+            let keyword is used when we want to use the variable and Const
+            keyword used when we have to define the constant value
+          </p>
+          <p className="define-text">
+            1) Let and Const doesn't support Hoisiting and var keyword does if
+            you want to learn more about the Hoisiting i will leave the link for
+            you below of W3 schools
+            <br />
+            <a href="https://www.w3schools.com/js/js_hoisting.asp">
+              https://www.w3schools.com/js/js_hoisting.asp
+            </a>
+            <br />
+            2) Can acces with in the Block (Block Scope ) unlike var keyword
           </p>
         </div>
         <buttton onClick={basicclickFunction} className="basic-button">
